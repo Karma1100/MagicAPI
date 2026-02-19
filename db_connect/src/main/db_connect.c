7778d797a929c15ba0b75274a1777dc7dd53bf66
1100Karma <ajdefafa@gmail.com>
2026-02-19 12:05:19 -0500

    PQconnect

--- db_connect/src/main/db_connect.c
+++ db_connect/src/main/db_connect.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libpq-fe.h>
-
+#include <string.h>
 
 int main()
 {
 	const char *conninfo = "host=/var/run/postgresql dbname=magic_cards user=host1";
-	PGconn *conn = PGconnectdb(conninfo);
-
+	PQconn *conn = PQconnectdb(conninfo);
 	
+	char* feedback;
+	
+	switch(PQstatus(conn))
+	{
+		case CONNECTION_STARTED:
+			feedback = "Waiting for connection";
+			break;		
+		case CONNECTION_MADE:
+			feedback = "Connection made";
+			break;
+		default:
+			feedback = "Nothing happening cheif";
+			break;
+	}	
 	return 0;
 
 }
